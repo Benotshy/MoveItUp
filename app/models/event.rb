bfb2d057bac621698fcf0c9b9ba7bbad0624550c
benchekroun othmane <othmanebenchekroun23@gmail.com>
2024-08-13 15:36:53 +0100

    Small modif on show.p & new pending page

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,6 @@ class Event < ApplicationRecord
   belongs_to :user
   belongs_to :type
 
+  has_many :participations
+  has_many :participants, through: :participations, source: :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_23_190318) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_12_202239) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -29,6 +29,16 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_23_190318) do
     t.datetime "updated_at", null: false
     t.index ["type_id"], name: "index_events_on_type_id"
     t.index ["user_id"], name: "index_events_on_user_id"
+  end
+
+  create_table "participations", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "event_id", null: false
+    t.string "status", default: "pending"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["event_id"], name: "index_participations_on_event_id"
+    t.index ["user_id"], name: "index_participations_on_user_id"
   end
 
   create_table "types", force: :cascade do |t|
@@ -65,6 +75,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_23_190318) do
 
   add_foreign_key "events", "types"
   add_foreign_key "events", "users"
+  add_foreign_key "participations", "events"
+  add_foreign_key "participations", "users"
   add_foreign_key "user_events", "events"
   add_foreign_key "user_events", "users"
 end
